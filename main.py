441d644fb300c6cdbf5ae8529eb34a7400f60bff
AKR-2803 <akr28032001@gmail.com>
2025-09-06 18:03:07 -0700

    search-agent basic run

--- main.py
+++ main.py
@@ -5,12 +5,34 @@ load_dotenv()
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
-from langchain_tavily import tavily_search
+from langchain_ollama import ChatOllama
+from langchain_tavily import TavilySearch
 
+tools = [TavilySearch()]
+
+llm = ChatOllama(model="gemma3:4b")
+react_prompt = hub.pull("hwchase17/react")
+
+# though named `agent` this is actually just a simple chain, i.e. a Runnable instance
+# video ref: @3:49 [https://successkpi.udemy.com/course/langchain/learn/lecture/52110075#overview]
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt,
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+chain = agent_executor
 
 def main():
-    print("Hello from langchain-course - react-search-agent")
-
+    result = chain.invoke(
+        input={
+            "input":"Search for 3 job listings for an AI engineer using langchain in Bay Area on linkedin and list thier details",
+        }
+    )
+    
+    print(result)
 
 if __name__ == "__main__":
     main()
